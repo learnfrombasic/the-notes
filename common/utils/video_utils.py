081af8e5bbf532a876df256294296c134d58b642
MinLee0210 <bastreet124@gmail.com>
2025-04-06 22:04:22 +0700

    add others

--- common/utils/video_utils.py
+++ common/utils/video_utils.py
@@ -1,7 +1,8 @@
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 
-def download_youtube_video(url: str, dest: str): 
+
+def download_youtube_video(url: str, dest: str):
     yt = YouTube(url, on_progress_callback=on_progress)
     ys = yt.streams.get_highest_resolution()
     ys.download(output_path=dest)
